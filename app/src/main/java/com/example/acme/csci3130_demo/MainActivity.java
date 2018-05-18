4333ae53c08156c5487cfcbc7dd422bbca080164
Peter Lee <peter@Fermati.local>
2018-05-18 11:24:21 -0300

    Made q3 changes

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+	/* 
+	   Function: sendMessage
+	   Called on send_button.onClick():
+	   ---------
+	   Grabs text from editText and displays it on the main textview.
+	 */
+	EditText editText = (EditText) findViewById(R.id.edit_message);
+	String message = editText.getText().toString();
+	TextView textview = (TextView) findViewById(R.id.textView_disp);
+	textview.setText(message);
+	
     }
 }
